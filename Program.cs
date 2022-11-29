5ddb4963d4eedc3b07d42681096c1e329289f95f
dobrov.mn <dobroff-72@inbox.ru>
2022-11-29 20:58:30 +0500

    Add final task

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+string [] arrayOne ;
+Console.WriteLine("Введите через пробел массив строк");
+    string text = Console.ReadLine();
+    arrayOne = text.Split(' ');
+    int size = arrayOne.Length;
+Console.WriteLine("Исходный массив:");
+PrintArray(arrayOne);
+Console.WriteLine();
+Console.WriteLine("Новый массив, содержащий элементы, размер которых не более 3:");
+
+if(GetSecondSize(arrayOne) == 0)   //Если нет элементов с 3 символами
+{
+    Console.WriteLine("Элементов, длина которых меньше 3 символов в данном массиве нет");
+}
+else
+{
+    string [] arrayTwo = NewArray(arrayOne);
+    PrintArray(arrayTwo);
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.Write("]");
+}
+
+int GetSecondSize(string[] arr)    // Размер нового массива
+{
+     int SecondSize = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i].Length <= 3)
+            {
+            SecondSize ++;
+            }
+    }
+    return SecondSize;
+}
+
+string[] NewArray(string[] arr)     // создание нового массива из первого
+{
+    string[] arrayTwo = new string[GetSecondSize(arrayOne)];
+    for (int i = 0, j =0; i < size; i++)
+    {
+        if (arr[i].Length <= 3)
+            {
+            arrayTwo[j] = arr[i];
+            j++;
+            }
+    }
+    return arrayTwo;
+}
+
